969e3dc69f264eaee3f7f44bb0afbd4896acea53
Jens Kristian <jenskristian@Sebastians-MacBook-Pro-2.local>
2023-05-08 10:24:48 +0200

    Test Get ALL Taks Insomnia/Postman

--- task-manager/routes/api.php
+++ task-manager/routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\ToolController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('tasks', TaskController::class);
+Route::apiResource('employees', EmployeeController::class);
+Route::apiResource('tools', ToolController::class);
