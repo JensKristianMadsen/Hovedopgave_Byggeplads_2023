7e7423d8f9d2e3b2dc7e4d4eae94d3bc1c360f88
Jens Kristian <jenskristian@Sebastians-MacBook-Pro-2.local>
2023-05-12 11:15:47 +0200

    HB-25 add  tryk-gamemføre task

--- task-manager/resources/js/components/TaskList.vue
+++ task-manager/resources/js/components/TaskList.vue
@@ -10,8 +10,9 @@
     </tr>
   </thead>
   <tbody>
-    <tr v-for="task in tasks" :key="task.id" >
-                    <td>{{ task.is_completed }}</td>
+    <tr v-for="task in tasks" :key="task.id" :class="{ 'completed': task.is_completed }">
+                    <td><input type="checkbox" v-model="task.is_completed" @change="taskCompletionCheck(task)"/>
+                    </td>
                     <td>{{ task.description }}</td>
                     <td>{{ task.tool_id }}</td>
                     <td>{{  task.employee_id }}</td>
@@ -41,11 +42,26 @@
         } catch (error) {
           console.error(error);
         }
+      },
+      methods: {
+        async taskCompletionCheck(task) {
+          try {
+            await axios.patch(`http://localhost:8000/api/tasks/${task.id}`, {
+              is_completed: task.is_completed
+            });
+          } catch (error) {
+            console.error(error);
+            task.is_completed = !task.is_completed;
+          }
+        }
       }
     
     }
     </script>
     
     <style>
-
+    .completed {
+      text-decoration: line-through;
+      color: gray;
+    }
     </style>
