3117969330ef4e481afa280e5e69025f256ba51e
Jens Kristian <jenskristian@Sebastians-MacBook-Pro-2.local>
2023-05-16 15:26:46 +0200

    HB-29 oprate Components hedder Task.vue

--- task-manager/resources/js/components/App.vue
+++ task-manager/resources/js/components/App.vue
@@ -6,11 +6,25 @@
 
 <script>
 import TaskList from './TaskList.vue';
+import axios from 'axios';
 export default {
     name: 'App',
     components: {
         TaskList
-    }
+    },
+      data() {
+        return {
+          tasks: []
+        };
+      },
+      async mounted() {
+        try {
+          const res = await axios.get('http://localhost:8000/api/tasks');
+          this.tasks = res.data;
+        } catch (error) {
+          console.error(error);
+        }
+      },
 };
 </script>
 
