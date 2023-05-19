6f0b16aaf2a5ad3623655b8b258ff596aa4131e0
Jens Kristian <jenskristian@Sebastians-MacBook-Pro-2.local>
2023-05-19 11:30:54 +0200

    HB-23 done 100

--- task-manager/resources/js/components/Task.vue
+++ task-manager/resources/js/components/Task.vue
@@ -6,18 +6,12 @@
   </div>
 
   <div class="mb-3">
-      <label for="tool" class="form-label">Tool</label>
+      <label for="toolr" class="form-label">Tool</label>
       <select class="form-control" id="tool" v-model="task.tool_id" required>
         <option v-for="tool in tools" :key="tool.id" :value="tool.id">{{ tool.item }}</option>
       </select>
     </div>
-
-    <div class="mb-3">
-      <label for="employee" class="form-label">Employee</label>
-      <select class="form-control" id="employee" v-model="task.employee_id" required>
-        <option v-for="employee in employees" :key="employee.id" :value="employee.id">{{ employee.name }}</option>
-      </select>
-    </div>
+    
   
   <button type="submit" class="btn btn-primary" @click="addTask">Submit</button>
 
@@ -36,11 +30,9 @@ data(){
     return {
         task: {
             description: '',
-            tool_id: '',
-            employee_id: ''
+            tool_id: ''
         },
-        tools: [],
-        employees: []
+        tools: []
     };
     
     },
@@ -49,14 +41,6 @@ data(){
     try {
       const res = await axios.get('http://localhost:8000/api/tools');
       this.tools = res.data;
-    } catch (error) {
-      console.error(error);
-    }
-  },
-  async mounted() {
-    try {
-      const res = await axios.get('http://localhost:8000/api/employees');
-      this.employees = res.data;
     } catch (error) {
       console.error(error);
     }
