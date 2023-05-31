1b6290b75bbb76b484843edd804b55d6f6f2f1c1
Jens Kristian <jenskristian@Sebastians-MacBook-Pro-2.local>
2023-05-31 11:43:02 +0200

    HB-33 done

--- task-manager/resources/js/components/Task.vue
+++ task-manager/resources/js/components/Task.vue
@@ -1,24 +1,69 @@
+
 <template>
-  <form>
-    <div class="mb-3">
-      <label for="taskDescription" class="form-label">Task Description</label>
-      <input type="text" class="form-control" id="taskDescription" v-model="task.description" required>
+
+  <div class="mt-10 sm:mt-0">
+    <div class="md:grid md:grid-cols-4 md:gap-6">
+      <div class="md:col-span-1">
+        <div class="px-4 sm:px-0">
+          <h3 class="text-lg font-medium leading-6 text-white">Personal Information</h3>
+          <p class="mt-1 text-sm text-white">Use a permanent address where you can receive mail.</p>
+        </div>
+      </div>
+      <div class="mt-5 md:col-span-2 md:mt-0">
+        <form >
+          <div class="overflow-hidden shadow sm:rounded-md">
+            <div class="bg-white px-4 py-5 sm:p-6">
+              <div class="grid grid-cols-4 gap-4">
+          
+                <div class="col-span-1 sm:col-span-1 lg:col-span-1">
+                  <label for="taskDescription" class="block text-sm font-medium text-gray-700">Task Description</label>
+                  <input type="text" v-model="task.description" name="taskDescription" id="taskDescription" autocomplete="address-level2" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" role="alert" required>
+                </div>
+
+
+  <!--
+                <div class="col-span-1 sm:col-span-1 lg:col-span-1">
+                  <label for="date" class="block text-sm font-medium text-gray-700">Date</label>
+                  <input datepicker   type="date" autocomplete="address-level1" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" required>
+                </div>
+-->
+
+             
+                <div  class="col-span-1 sm:col-span-1 lg:col-span-1">
+                  <label for="tool" class="block text-sm font-medium text-gray-700">Tool</label>
+                  <select v-model="task.tool_id" id="tool" name="tool" autocomplete="tool-item" class="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
+                    <option disabled value="">Please select one</option>
+                    <option v-for="tool in availableTools" :key="tool.id" :value="tool.id">{{ tool.item }}</option>
+                  </select> 
+                </div>
+
+                <div  class="col-span-1 sm:col-span-1 lg:col-span-1">
+                  <label for="employee" class="block text-sm font-medium text-gray-700">Employee</label>
+                  <select v-model="task.employee_id" id="employee" name="employee" autocomplete="employee-name" class="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
+                    <option disabled value="">Please select one</option>
+                    <option v-for="employee in employees" :key="employee.id" :value="employee.id">{{ employee.name }}</option>
+                  </select> 
+                </div>
+              </div>
+
+
+            </div>
+            <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+              <button type="submit"  @click="addTask"
+                class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Save</button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
-    <div class="mb-3">
-      <label for="tool" class="form-label">Tool</label>
-      <select class="form-control" id="tool" v-model="task.tool_id" required>
-          <option v-for="tool in availableTools" :key="tool.id" :value="tool.id">{{ tool.item }}</option>
-        </select>
-    </div>
-    <div class="mb-3">
-      <label for="employee" class="form-label">Employee</label>
-      <select class="form-control" id="employee" v-model="task.employee_id" required>
-          <option v-for="employee in employees" :key="employee.id" :value="employee.id">{{ employee.name }}</option>
-        </select>
-    </div>
-    <button type="submit" class="btn btn-primary" @click="addTask">Submit</button>
-  </form>
-</template>
+  </div>
+  
+
+  </template>
+
+
+
+
 
 <script>
   import axios from 'axios';
@@ -89,5 +134,13 @@
 </script>
 
 <style>
+
+
+
+#app {
+font-family: "Avenir", Helvetica, Arial, sans-serif;
+-webkit-font-smoothing: antialiased;
+-moz-osx-font-smoothing: grayscale;
+}
 
 </style>
--- task-manager/resources/js/components/TaskListTest.vue
+++ task-manager/resources/js/components/TaskListTest.vue
@@ -1,324 +1,55 @@
-<!--
+
+
+
+
+
 <template>
-  <div>
-      <Task />
-  </div>
-  <div>
+  <form>
+    <div class="mb-3">
+      <label for="taskDescription" class="form-label">Task Description</label>
+      <input type="text" class="form-control" id="taskDescription" v-model="task.description" required>
+    </div>
+    <div class="mb-3">
+      <label for="tool" class="form-label">Tool</label>
+      <select class="form-control" id="tool" v-model="task.tool_id" required>
+          <option v-for="tool in availableTools" :key="tool.id" :value="tool.id">{{ tool.item }}</option>
+        </select>
+    </div>
+    <div class="mb-3">
+      <label for="employee" class="form-label">Employee</label>
+      <select class="form-control" id="employee" v-model="task.employee_id" required>
+          <option v-for="employee in employees" :key="employee.id" :value="employee.id">{{ employee.name }}</option>
+        </select>
+    </div>
+    <button type="submit" class="btn btn-primary" @click="addTask">Submit</button>
+  </form>
+</template>
+
+
+<template>
+
+  <!--Navbar-->
+  <nav class="relative w-full flex flex-wrap items-center justify-between py-3 bg-gray-100 text-gray-500 hover:text-gray-700 focus:text-gray-700 shadow-lg">
+    <div class="container-fluid w-full flex flex-wrap items-center justify-between px-6">
+        <div class="container-fluid">
+            <a class="text-xl text-black font-semibold" href="#">Navbar</a> 
+        </div>
+    </div>
+</nav>
+<!--Navbar-->
+<div class="container w-100 m-auto text-center mt-3">
+    <h1 class="text-danger">Laravel vue todo list</h1>
+<!--AddTaskForm-->
+<Task />
+<!--AddTaskForm-->
+
+<!--TaskList-->
 <TaskList />
-  </div>
-  <div>
-    <TaskCompletedList />
-  </div>
+<!--askCompletedList -->
+<TaskCompletedList />
+
+
+</div>
+
+
 </template>
-
-<script>
-import TaskCompletedList from './TaskCompletedList.vue';
-import TaskList from './TaskList.vue';
-import Task from './Task.vue';
-import axios from 'axios';
-
-export default {
-
-  name: 'App',
-  components: {
-      TaskList,
-      Task,
-      TaskCompletedList
-  },
-  data() {
-    return {
-      tasks: [],
-      tools: [],
-      employees: []
-    };
-  },
-    async mounted() {
-      try {
-        const [taskRes, toolRes, employeeRes] = await Promise.all([
-          axios.get('http://localhost:8000/api/tasks'),
-          axios.get('http://localhost:8000/api/tools'),
-          axios.get('http://localhost:8000/api/employees')
-        ]);
-        this.tools = toolRes.data;
-        this.employees = employeeRes.data;
-        this.tasks = taskRes.data.map(task => ({ ...task,
-          isEditing: false
-        }));
-      } catch (error) {
-        console.error(error.message);
-      }
-    },
-};
-</script>
-
-<style scoped>
-</style>
--->
-
-<!-- del taskList
-<template>
-  <table class="table">
-    <thead>
-      <tr>
-        <th scope="col">Completed</th>
-        <th scope="col">Description</th>
-        <th scope="col">Tool</th>
-        <th scope="col">Employee</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="task in TasksNotCompleted" :key="task.id" :class="{ 'completed': task.is_completed }">
-        <td><input type="checkbox" v-model="task.is_completed" @change="taskCompletionCheck(task)" /> </td>
-
-        <td v-if="task.isEditing">
-          <input type="text" v-model="task.description" />
-        </td>
-        <td v-else>{{ task.description }}</td>
-
-        <td v-if="task.isEditing">
-          <select v-model="task.tool_id" @change="updateTask(task)">
-            <option v-for="tool in tools" :key="tool.id" :value="tool.id">{{ tool.item }}</option>
-          </select>
-        </td>
-        <td v-else>{{ getToolItem(task.tool_id) }}</td>
-        
-        <td v-if="task.isEditing">
-          <select v-model="task.employee_id" @change="updateTask(task)">
-            <option v-for="employee in employees" :key="employee.id" :value="employee.id">{{ employee.name }}</option>
-          </select>
-        </td>
-        <td v-else>{{ getEmployeeName(task.employee_id) }}</td>
-        <td>
-          <button @click="task.isEditing ? updateTask(task) : task.isEditing = !task.isEditing">{{ task.isEditing ? 'Save' : 'Edit' }}</button>
-        </td>
-        <td> <button @click="deleteTask(task)">Delete</button> </td>
-      </tr>
-    </tbody>
-  </table>
-</template>
-    
-<script>
-  import axios from 'axios';
-  export default {
-    props: {
-      tasks: Array,
-      tools: Array,
-      employees: Array
-    },
-    data() {
-      return {
-        tasks: [],
-        tools: [],
-        employees: []
-      };
-    },
-    computed:{
-      TasksNotCompleted() {
-      return this.tasks.filter(task => !task.is_completed);
-    }
-    },
-    async mounted() {
-      try {
-        const [taskRes, toolRes, employeeRes] = await Promise.all([
-          axios.get('http://localhost:8000/api/tasks'),
-          axios.get('http://localhost:8000/api/tools'),
-          axios.get('http://localhost:8000/api/employees')
-        ]);
-        this.tools = toolRes.data;
-        this.employees = employeeRes.data;
-        this.tasks = taskRes.data.map(task => ({ ...task,
-          isEditing: false
-        }));
-      } catch (error) {
-        console.error(error.message);
-      }
-    },
-    methods: {
-
-      getEmployeeName(id) {
-    const employee = this.employees.find(employee => employee.id === id);
-    return employee ? employee.name : 'Unknown';
-  },
-  getToolItem(id) {
-    const tool = this.tools.find(tool => tool.id === id);
-    return tool ? tool.item : 'Unknown';
-  },
-      async taskCompletionCheck(task) {
-        try {
-           await axios.patch(`http://localhost:8000/api/tasks/${task.id}`, {
-            is_completed: task.is_completed
-          });
-        } catch (error) {
-          console.error(error.message);
-          task.is_completed = !task.is_completed;
-        }
-      },
-      async updateTask(task) {
-        try {
-          await axios.put(`http://localhost:8000/api/tasks/${task.id}`, {
-            description: task.description,
-            tool_id: task.tool_id,
-            employee_id: task.employee_id
-          });
-        } catch (error) {
-          console.error(error.message);
-        }
-        task.isEditing = false;
-      },
-      async deleteTask(task) {
-        if (!confirm(`Confirm to delete task ${task.description}?`)) {
-          return;
-        }
-        try {
-          await axios.delete(`http://localhost:8000/api/tasks/${task.id}`)
-          const taskIndex = this.tasks.indexOf(task);
-          if (taskIndex > -1) {
-            this.tasks = this.tasks.filter(t => t.id !== task.id);
-          }
-        } catch (error) {
-          console.error(error.message);
-        }
-      }
-    }
-  }
-</script>
-    
-<style>
-  .completed {
-    text-decoration: line-through;
-    color: gray;
-  }
-</style>
--->
-
-
-<!--det 3
-
-<template>
-  <table class="table">
-    <thead>
-      <tr>
-        <th scope="col">Completed</th>
-        <th scope="col">Description</th>
-        <th scope="col">Tool</th>
-        <th scope="col">Employee</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="task in tasksNotCompleted" :key="task.id" :class="{ 'completed': task.is_completed }">
-        <td><input type="checkbox" v-model="task.is_completed" @change="taskCompletionCheck(task)" /> </td>
-
-        <td v-if="task.isEditing">
-          <input type="text" v-model="task.description" />
-        </td>
-        <td v-else>{{ task.description }}</td>
-
-        <td v-if="task.isEditing">
-          <select v-model="task.tool_id" @change="updateTask(task)">
-            <option v-for="tool in tools" :key="tool.id" :value="tool.id">{{ tool.item }}</option>
-          </select>
-        </td>
-        <td v-else>{{ getToolItem(task.tool_id) }}</td>
-        
-        <td v-if="task.isEditing">
-          <select v-model="task.employee_id" @change="updateTask(task)">
-            <option v-for="employee in employees" :key="employee.id" :value="employee.id">{{ employee.name }}</option>
-          </select>
-        </td>
-        <td v-else>{{ getEmployeeName(task.employee_id) }}</td>
-        <td>
-          <button @click="task.isEditing ? updateTask(task) : task.isEditing = !task.isEditing">{{ task.isEditing ? 'Save' : 'Edit' }}</button>
-        </td>
-        <td> <button @click="deleteTask(task)">Delete</button> </td>
-      </tr>
-    </tbody>
-  </table>
-</template>
-    
-<script>
-  import axios from 'axios';
-  export default {
-    data() {
-      return {
-        tasks: [],
-        tools: [],
-        employees: []
-      };
-    },
-    computed:{
-      tasksNotCompleted() {
-      return this.tasks.filter(task => !task.is_completed);
-    }
-    },
-    async mounted() {
-      try {
-        const [taskRes, toolRes, employeeRes] = await Promise.all([
-          axios.get('http://localhost:8000/api/tasks'),
-          axios.get('http://localhost:8000/api/tools'),
-          axios.get('http://localhost:8000/api/employees')
-        ]);
-        this.tools = toolRes.data;
-        this.employees = employeeRes.data;
-        this.tasks = taskRes.data.map(task => ({ ...task,
-          isEditing: false
-        }));
-      } catch (error) {
-        console.error(error.message);
-      }
-    },
-    methods: {
-
-      getEmployeeName(id) {
-    const employee = this.employees.find(employee => employee.id === id);
-    return employee ? employee.name : 'Unknown';
-  },
-  getToolItem(id) {
-    const tool = this.tools.find(tool => tool.id === id);
-    return tool ? tool.item : 'Unknown';
-  },
-      async taskCompletionCheck(task) {
-        try {
-          await axios.patch(`http://localhost:8000/api/tasks/${task.id}`, {
-            is_completed: task.is_completed
-          });
-        } catch (error) {
-          console.error(error.message);
-          task.is_completed = !task.is_completed;
-        }  
-      },
-      async updateTask(task) {
-        try {
-          await axios.put(`http://localhost:8000/api/tasks/${task.id}`, {
-            description: task.description,
-            tool_id: task.tool_id,
-            employee_id: task.employee_id
-          });
-        } catch (error) {
-          console.error(error.message);
-        }
-        task.isEditing = false;
-      },
-      async deleteTask(task) {
-        if (!confirm(`Confirm to delete task ${task.description}?`)) {
-          return;
-        }
-        try {
-          await axios.delete(`http://localhost:8000/api/tasks/${task.id}`)
-          const taskIndex = this.tasks.indexOf(task);
-          if (taskIndex > -1) {
-            this.tasks = this.tasks.filter(t => t.id !== task.id);
-          }
-        } catch (error) {
-          console.error(error.message);
-        }
-      }
-    }
-  }
-</script>
-    
-<style>
-  .completed {
-    text-decoration: line-through;
-    color: gray;
-  }
-</style>-->
